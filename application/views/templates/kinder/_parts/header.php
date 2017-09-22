30376606fb09c186a4cd6680451b4d73c030ba80
ntnt <trang.nguyen@whl.travel>
2017-09-22 17:01:32 +0700

    a

--- application/views/templates/kinder/_parts/header.php
+++ application/views/templates/kinder/_parts/header.php
@@ -2,6 +2,7 @@
 <head>
     <!-- Basic page needs ================================================== -->
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <link rel="shortcut icon" href="/assets/kinder/image/favicon.png" type="image/png" />
     <!-- Title and description ================================================== -->
